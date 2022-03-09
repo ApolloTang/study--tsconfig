12ed46c117f9b7636f570bbab965ea8c564af818
Apollo Tang <tang.apollo@gmail.com>
2022-03-09 16:38:19 -0500

    fix typo in folder name

--- isolatedModules/tsc/1c-isolateModules-true/src/export-import.ts
+++
@@ -1,8 +0,0 @@
-import { someFunction } from "./some-module";
-import { fooType } from "./some-module";
-
-export { someFunction };
-export { fooType };  // <--- IDE tells us this can lead to mistake
-//       ^^^^^^^
-//       [tsserver 1205] Re-exporting a type when the '--isolatedModules'
-//       flag is provided requires using 'export type'
--- isolatedModules/tsc/1c-isolateModules-true/src/some-module.ts
+++
@@ -1,5 +0,0 @@
-const someFunction = () => 'return of someFunction'
-type fooType = string
-
-export { someFunction }
-export { fooType }
